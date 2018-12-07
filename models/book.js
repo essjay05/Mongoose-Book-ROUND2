616b59c6af1e8627937a152afdafd97e18a8cc53
Joy <stepjoyous@gmail.com>
2018-12-07 14:30:02 -0800

    Created book model

--- models/book.js
+++ models/book.js
@@ -1,1 +1,16 @@
 // Require mongoose in here.
+const mongoose = require('mongoose');
+
+// Create book schema;
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema ({
+    title: String,
+    author: String,
+    image: String,
+    releaseDate: Date
+})
+
+const Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
